662a69b76187d6c9805bf52fb7404d8598fa30bf
Argude01 <30556136+Argude01@users.noreply.github.com>
2017-10-15 23:09:25 +0200

    Add files via upload

---
+++ dataset.sql
@@ -0,0 +1,10 @@
+INSERT INTO categories(category) VALUES ('Food');
+INSERT INTO categories(category) VALUES ('Clothes');
+
+INSERT INTO products(product, idCategory) VALUES ('Milk', '1');
+INSERT INTO products(product, idCategory) VALUES ('Pants', '2');
+
+INSERT INTO brands(brand, idProduct) VALUES ('Nike', '2');
+INSERT INTO brands(brand, idProduct) VALUES ('Adidas', '2');
+INSERT INTO brands(brand, idProduct) VALUES ('Sula', '1');
+INSERT INTO brands(brand, idProduct) VALUES ('Leyde', '1');
